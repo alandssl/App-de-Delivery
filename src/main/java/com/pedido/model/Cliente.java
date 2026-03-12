3847f8840ba55371ff653bb4f7d5abd00d70ba75
alandssl <alan.ds.0210@gmail.com>
2026-03-12 14:23:29 -0300

    Service do Cliente feio

--- src/main/java/com/pedido/model/Cliente.java
+++ src/main/java/com/pedido/model/Cliente.java
@@ -10,13 +10,17 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Entity
 @Table(name = "clientes")
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class Cliente {
 
     @Id
--- src/main/java/com/pedido/service/ClienteService.java
+++ src/main/java/com/pedido/service/ClienteService.java
@@ -1,6 +1,14 @@
 package com.pedido.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
+
+import com.pedido.dto.ClienteRequestDTO;
+import com.pedido.dto.ClienteResponseDTO;
+import com.pedido.model.Cliente;
+import com.pedido.repository.ClienteRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -8,4 +16,64 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ClienteService {
 
+    private final ClienteRepository repository;
+
+    public ClienteResponseDTO criar(ClienteRequestDTO dto) {
+        Cliente cliente = new Cliente();
+        cliente.setName(dto.getName());
+        cliente.setTelefone(dto.getTelefone());
+        cliente.setEmail(dto.getEmail());
+
+        Cliente clienteSalvo = repository.save(cliente);
+
+        return toResponseDTO(clienteSalvo);
+
+    }
+
+    public ClienteResponseDTO buscarPorId(Long id) {
+        Cliente cliente = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+
+        return toResponseDTO(cliente);
+    }
+
+    public ClienteResponseDTO atualizar(Long id, ClienteRequestDTO dto) {
+        Cliente cliente = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+
+        cliente.setName(dto.getName());
+        cliente.setTelefone(dto.getTelefone());
+        cliente.setEmail(dto.getEmail());
+
+        Cliente clienteAtualizado = repository.save(cliente);
+
+        return toResponseDTO(clienteAtualizado);
+    }
+
+    public void deletar(Long id) {
+        Cliente cliente = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+
+        repository.delete(cliente);
+    }
+
+    public List<ClienteResponseDTO> ListarTodos(){
+        List<Cliente> clientes = repository.findAll();
+        return clientes.stream()
+                .map(this::toResponseDTO)
+                .collect(Collectors.toList());
+    }
+
+
+    private ClienteResponseDTO toResponseDTO(Cliente cliente) {
+
+        ClienteResponseDTO dto = new ClienteResponseDTO();
+
+        dto.setId(cliente.getId());
+        dto.setName(cliente.getName());
+        dto.setTelefone(cliente.getTelefone());
+        dto.setEmail(cliente.getEmail());
+
+        return dto;
+    }   
 }
